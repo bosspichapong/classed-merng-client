e469a7fea2041ecb9dcdcc969d5d1303533ab996
bosspichapong <42568992+bosspichapong@users.noreply.github.com>
2021-08-13 12:48:54 +0700

    edited proxy and server base URL

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-    uri: 'http://localhost:5000'
+    uri: 'https://tranquil-taiga-99340.herokuapp.com/'
 });
 
 const authLink = setContext(() => {
